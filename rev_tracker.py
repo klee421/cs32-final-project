4e75e299c3687b373506d82d74f1371ea3464ddf
klee421 <102482222+klee421@users.noreply.github.com>
2022-04-19 13:58:21 -0400

    Update rev_tracker.py

--- rev_tracker.py
+++ rev_tracker.py
@@ -1,34 +1,17 @@
-# convert contracts in pdf format into a client-by-client monthly subscription fee table
-    # extract from contract pdfs the data required to build the table
-        # convert contract pdfs into strings
-            # convert contract pdfs to image files
-            # convert the content of the image files into strings
-        # extract from strings the data required to build the table
-            # extract client names
-            # extract monthly subscription fees
-            # extract the month and year of the start of the subscription
-            # extract the montha and year of the end of the subscription
-
-    # use these data to build the table
-        # build a list of dictionaries, where keys are the months and values are the subscription fees
-        # convert this list of dictionaries into a csv file
-
-
-import sys
-from rev_tracker_lib import pdf_to_text, extract_name, extract_fee, extract_start, extract_duration, extract_period, extract_data
+from rev_tracker_lib_extracting import pdf_to_text, extract_name, extract_product, extract_fee, extract_start, extract_duration, extract_period, extract_individual, extract
+from rev_tracker_lib_polishing import get_integerized_date, get_date_range, build_keys, polish
+from rev_tracker_lib_exporting import export
 
 
 def main():
-    if len(sys.argv) <= 1:
-        sys.exit("need to input at least one contract in pdf")
+    # extract relevant data from contracts
+    list_unpolished = extract()
 
-    list_unpolished = []
+    # polish the data so that it can be easily exported into a spreadsheet
+    list_polished = polish(list_unpolished)
     
-    for contract in sys.argv[1:]:
-        data_unpolished = extract_data(contract)  
-        list_unpolished.append(data_unpolished)
-
-    print(list_unpolished)
+    # export the polished data into an excel workbook pre-coded to automatically run analytics on the revenue data being exported
+    export(list_polished)
 
 if __name__ == '__main__':
     main()
